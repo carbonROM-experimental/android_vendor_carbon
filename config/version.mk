a4d6dd407fc137d3fbf0c7d871372040d2a55af2
SirRGB <sirrgb@amyrom.ml>
2022-08-29 15:28:26 +0200

    config: add in gapps

--- config/version.mk
+++ config/version.mk
@@ -9,6 +9,22 @@ endif
 TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
 TARGET_PRODUCT_SHORT := $(subst carbon_,,$(TARGET_PRODUCT_SHORT))
 
-CARBON_VERSION := $(CARBON_REVISION)-$(CARBON_CODENAME)-$(CARBON_BUILDTYPE)-$(TARGET_PRODUCT_SHORT)-$(shell date -u +%Y%m%d-%H%M)
+# Chocolate
+ifeq ($(GAPPS),true)
+$(GAPPS will be included in the build)
+CARBON_GAPPS := CHOCOLATE
+ifeq ($(GAPPS_ARM64),)
+$(warning GAPPS_ARM64 is not set, it defaulting to 64bit)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+endif
+ifeq ($(GAPPS_ARM64), false)
+$(warning arm64 support is unavailable, setting GAPPS_ARM64 to false)
+$(call inherit-product, vendor/gapps/arm/arm-vendor.mk)
+endif
+else
+CARBON_GAPPS := VANILLA
+endif
+
+CARBON_VERSION := $(CARBON_REVISION)-$(CARBON_CODENAME)-$(CARBON_GAPPS)-$(CARBON_BUILDTYPE)-$(TARGET_PRODUCT_SHORT)-$(shell date -u +%Y%m%d-%H%M)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID="$(BUILD_ID)-$(BUILD_USERNAME)@$(BUILD_HOSTNAME)"
